c1b29bc1fe52b9bc6dd5939a229f849a8d2592c2
ATECHPCS <125108010+ATECHPCS@users.noreply.github.com>
2026-08-12 15:15:05 -0400

    Add reviewed enrichment apply actions

--- custom/grocy_AI/routes.php
+++ custom/grocy_AI/routes.php
@@ -12,4 +12,5 @@ $app->group('/api/grocy-ai', function (RouteCollectorProxy $group)
 {
 	$group->get('/status', [GrocyAiApiController::class, 'Status']);
 	$group->get('/products/enrich/upc/{upc}', [GrocyAiApiController::class, 'EnrichByUpc']);
+	$group->get('/images/{token}', [GrocyAiApiController::class, 'FetchImage']);
 })->add(new CorsMiddleware($container, $app->getResponseFactory()))->add(new JsonMiddleware($container, $app->getResponseFactory()));
--- custom/grocy_AI/src/GrocyAiApiController.php
+++ custom/grocy_AI/src/GrocyAiApiController.php
@@ -36,4 +36,37 @@ class GrocyAiApiController extends BaseApiController
 			return $this->GenericErrorResponse($response, $ex->getMessage(), 502);
 		}
 	}
+
+	public function FetchImage(Request $request, Response $response, array $args): Response
+	{
+		User::CheckPermission($request, User::PERMISSION_MASTER_DATA_EDIT);
+
+		try
+		{
+			$image = (new GrocyAiService())->FetchImage($args['token']);
+			$response->getBody()->write($image['body']);
+			$extension = [
+				'image/png' => 'png',
+				'image/webp' => 'webp'
+			][$image['content_type']] ?? 'jpg';
+
+			return $response
+				->withHeader('Cache-Control', 'private, no-store')
+				->withHeader('Content-Type', $image['content_type'])
+				->withHeader('Content-Disposition', 'inline; filename="grocy-ai-product.' . $extension . '"')
+				->withHeader('X-Content-Type-Options', 'nosniff');
+		}
+		catch (\InvalidArgumentException $ex)
+		{
+			return $this->GenericErrorResponse($response, $ex->getMessage(), 400);
+		}
+		catch (\LogicException $ex)
+		{
+			return $this->GenericErrorResponse($response, $ex->getMessage(), 503);
+		}
+		catch (\RuntimeException $ex)
+		{
+			return $this->GenericErrorResponse($response, $ex->getMessage(), 502);
+		}
+	}
 }
